e0828fb2155bf68da9d862dae714407ec43c2c37
JoelTejerina <catrieltejerina@gmail.com>
2022-06-20 19:31:39 -0300

    Se agrega proyecto

---
+++ curso/ejerciciouno/javabasico/TiposVariables.java
@@ -0,0 +1,34 @@
+package curso.ejerciciouno.javabasico;
+
+public class TiposVariables {
+    public static void main(String[] args) {
+
+        //Enteros
+        byte variable1 = 5;
+        System.out.println(variable1);
+        short variable2 = 12;
+        System.out.println(variable2);
+        int variable3 = 322;
+        System.out.println(variable3);
+        long variable4 = 23232323232L;
+        System.out.println(variable4);
+
+        //Decimales
+        double variable5 = 23.2d;
+        System.out.println(variable5);
+        float variable6 = 232.2323f;
+        System.out.println(variable6);
+
+        //Booleano
+        boolean variable7 = true;
+        System.out.println(variable7);
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+        //Texto
+        char variable9 = 's';
+        System.out.println(variable9);
+        String variable10 = "Hola";
+        System.out.println(variable10);
+    }
+}
